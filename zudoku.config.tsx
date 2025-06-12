45ff18d0e01ff633b2b4315632bb2053d6841bc1
victormer <victormerinokosina@protonmail.com>
2025-06-12 17:13:38 +0200

    initial commit

--- zudoku.config.tsx
+++ zudoku.config.tsx
@@ -3,32 +3,19 @@ import type { ZudokuConfig } from "zudoku";
 const config: ZudokuConfig = {
   page: {
     logo: {
-      src: { light: "/logo-light.svg", dark: "/logo-dark.svg" },
-      alt: "Zudoku",
-      width: "130px",
+      src: { light: "/logo.svg", dark: "/logo.svg" },
+      alt: "Buu AI",
+      width: "100px",
     },
   },
   topNavigation: [
-    { id: "docs", label: "Documentation" },
     { id: "api", label: "API Reference" },
   ],
-  sidebar: {
-    docs: [
-      {
-        type: "category",
-        label: "Overview",
-        items: ["docs/introduction", "docs/example"],
-      },
-    ],
-  },
-  redirects: [{ from: "/", to: "/docs/introduction" }],
+  redirects: [{ from: "/", to: "/api" }],
   apis: {
     type: "file",
-    input: "./apis/openapi.yaml",
+    input: "./apis/openapi.json",
     navigationId: "api",
-  },
-  docs: {
-    files: "/pages/**/*.{md,mdx}",
   },
 };
 
